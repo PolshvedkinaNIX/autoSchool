b596fc205d333b64d6452b6db1b776927d481310
PolshvedkinaNIX <yuliia.polshvedkina@nixsolutions.com>
2020-12-16 15:19:39 +0200

    First program is created.

---
+++ src/main/java/com/tasks/task1/Main.java
@@ -0,0 +1,16 @@
+package com.tasks.task1;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, world!");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input name: ");
+        String name = in.nextLine();
+        System.out.printf("Hello, %s",name);
+    }
+
+
+}
