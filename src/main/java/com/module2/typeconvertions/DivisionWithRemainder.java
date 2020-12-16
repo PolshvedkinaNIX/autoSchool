0847500686caf87c2a2de58c0cc7a4077a9b5dd7
PolshvedkinaNIX <yuliia.polshvedkina@nixsolutions.com>
2020-12-16 18:35:45 +0200

    Next task is done.

---
+++ src/main/java/com/module2/typeconvertions/DivisionWithRemainder.java
@@ -0,0 +1,28 @@
+package com.module2.typeconvertions;
+import java.io.*;
+/*
+В переменных q и w хранятся два натуральных числа. Создайте программу,
+выводящую на экран результат деления q на w с остатком.
+Пример вывода программы (для случая, когда в q хранится 21, а в w хранится 8):
+21 / 8 = 2 и 5 в остатке
+*/
+
+public class DivisionWithRemainder
+{
+    public static void main(String[] args) throws IOException {
+        InputStream inputStream = System.in;
+        Reader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        System.out.println("Input q: ");
+        String sQ = bufferedReader.readLine();
+        System.out.println("Input w: ");
+        String sW = bufferedReader.readLine();
+        int q = Integer.parseInt(sQ);
+        int w = Integer.parseInt(sW);
+        int quotient = q / w;
+        int remainder = q - quotient*w;
+
+        System.out.printf("%d / %d = %d и %d в остатке\n", q, w, quotient, remainder);
+    }
+}
