bfa7b3c70dca6dec496cbe40d3a817eae3cbc555
PolshvedkinaNIX <yuliia.polshvedkina@nixsolutions.com>
2020-12-24 19:42:38 +0200

    Everything is almost ready =)

--- src/main/java/com/module3/figure/Test.java
+++ src/main/java/com/module3/figure/Test.java
@@ -1,11 +1,16 @@
 package com.module3.figure;
-import java.io.IOException;
-import java.util.Random;
 
 public class Test
 {
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args)
     {
+        Square A = new Square(1, 1, 1, 3);
+        System.out.println(A.getArea());
+        System.out.println(A.getSide().getLength());
+        System.out.println(A.getSide().getStart().getX());
+        System.out.println(A.getSide().getStart().getY());
+        System.out.println(A.getSide().getEnd().getX());
+        System.out.println(A.getSide().getEnd().getY());
 
     }
 }
