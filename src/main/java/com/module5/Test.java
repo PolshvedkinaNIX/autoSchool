27b0d917a408d3d715eda3ed2e781c0e4f1b25fa
PolshvedkinaNIX <yuliia.polshvedkina@nixsolutions.com>
2021-01-08 18:00:43 +0200

    Task 5 is started.

---
+++ src/main/java/com/module5/Test.java
@@ -0,0 +1,9 @@
+package com.module5;
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+
+        ArrayList list = new ArrayList(); // todo попробовать, можно ли совать элементы разных типов
+    }
+}
